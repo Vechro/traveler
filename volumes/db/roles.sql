0501719d1c6b57540954e4e478c78dfa9ea1b67f
vechro <vechro@vech.ro>
2022-11-06 18:11:13 +0200

    Fix up configs

--- volumes/db/roles.sql
+++ volumes/db/roles.sql
@@ -3,4 +3,6 @@
 
 ALTER USER authenticator WITH PASSWORD :'pgpass';
 ALTER USER pgbouncer WITH PASSWORD :'pgpass';
-ALTER USER auth_admin WITH PASSWORD :'pgpass';
+-- NOTE: These usernames seems to be set in stone by supabase/postgres
+ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
+ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
