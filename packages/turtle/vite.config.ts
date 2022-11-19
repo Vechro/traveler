3e12d787afa5218e5206c5fc2d4e261df08b38e8
vechro <vechro@vech.ro>
2022-11-19 12:57:27 +0200

    Fix turtle config

--- packages/turtle/vite.config.ts
+++ packages/turtle/vite.config.ts
@@ -2,18 +2,16 @@ import minifyHTML from "rollup-plugin-html-literals";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    build: {
-      lib: {
-        entry: "src/index.ts",
-        name: "Turtle",
-        formats: ["es"],
-      },
-      rollupOptions: {
-        external: /^lit/,
-      },
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "Turtle",
+      formats: ["es"],
     },
-    plugins: [minifyHTML()],
-  };
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
+  plugins: [minifyHTML()],
 });
