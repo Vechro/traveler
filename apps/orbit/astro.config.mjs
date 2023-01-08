de4ea11d5475110d8722e53cfdc29247885da3ee
vechro <vechro@vech.ro>
2023-01-08 11:30:33 +0200

    Dedupe three.js

--- apps/orbit/astro.config.mjs
+++ apps/orbit/astro.config.mjs
@@ -6,6 +6,10 @@ import minifyHTML from "rollup-plugin-html-literals";
 export default defineConfig({
   integrations: [lit()],
   vite: {
+    resolve: {
+      // <model-viewer> depends on three.js as well
+      dedupe: ["three"],
+    },
     plugins: [minifyHTML()],
   },
   server: {
