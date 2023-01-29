d21a985e96091642f5f5e6dd500703728a4b9c0b
vechro <vechro@vech.ro>
2023-01-29 11:39:14 +0200

    Configure apisix

--- apps/orbit/src/utilities/api.ts
+++ apps/orbit/src/utilities/api.ts
@@ -3,14 +3,13 @@ import { PostgrestClient } from "@supabase/postgrest-js";
 import type { Database } from "&/database";
 
 const API_URL = "http://localhost:9080";
-const API_ANON =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
+const API_ANON = "anon-key";
 
 export const getAuth = () =>
   new GoTrueClient({
     url: `${API_URL}/auth/v1`,
     headers: {
-      accept: "json",
+      Accept: "application/json",
       apikey: API_ANON,
     },
   });
@@ -19,4 +18,4 @@ export const getPostgrest = (options?: ConstructorParameters<typeof PostgrestCli
   new PostgrestClient<Database>(`${API_URL}/rest/v1`, options);
 
 export const getPostgrestWithSession = ({ access_token }: Session) =>
-  getPostgrest({ headers: { Authorization: `Bearer ${access_token}` } });
+  getPostgrest({ headers: { Authorization: `Bearer ${access_token}`, apikey: API_ANON } });
