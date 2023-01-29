cbdb31ceb4c7e8e5770a2e9ad8cd3f03740fd38c
vechro <vechro@vech.ro>
2023-01-29 21:09:47 +0200

    Use relative urls

--- apps/orbit/src/utilities/api.ts
+++ apps/orbit/src/utilities/api.ts
@@ -2,11 +2,9 @@ import { GoTrueClient, type Session } from "@supabase/gotrue-js";
 import { PostgrestClient } from "@supabase/postgrest-js";
 import type { Database } from "&/database";
 
-const API_URL = "http://localhost:9080";
-
 export const getAuth = () =>
   new GoTrueClient({
-    url: `${API_URL}/auth/v1`,
+    url: `/auth/v1`,
     headers: {
       Accept: "application/json",
       apikey: import.meta.env.PUBLIC_ANON_KEY,
@@ -14,7 +12,7 @@ export const getAuth = () =>
   });
 
 export const getPostgrest = (options?: ConstructorParameters<typeof PostgrestClient>[1]) =>
-  new PostgrestClient<Database>(`${API_URL}/rest/v1`, options);
+  new PostgrestClient<Database>(`/rest/v1`, options);
 
 export const getPostgrestWithSession = ({ access_token }: Session) =>
   getPostgrest({ headers: { Authorization: `Bearer ${access_token}`, apikey: import.meta.env.PUBLIC_ANON_KEY } });
